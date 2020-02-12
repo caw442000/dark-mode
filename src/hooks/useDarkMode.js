3d909aaf8d367be700066b4e8882c251e3dacdcc
cedric-winbush <cawinbushjr@gmail.com>
2020-02-12 14:28:30 -0600

    MVP add useEffect and replace useState on Navbar

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,20 +1,22 @@
 import { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
-export const useDarkMode = (key) => {
+export const useDarkMode = (key, initialValue) => {
 
-  const [value, setValue] = useLocalStorage(key)
+  const [value, setValue] = useLocalStorage(key, initialValue)
 
   useEffect(() => {
 
+    
+
     if (value === true) {
 
-      // document.body.classList.add("dark-mode");
+      document.body.classList.add("dark-mode");
 
 
     } else {
 
-      // document.body.classList.remove("dark-mode");
+      document.body.classList.remove("dark-mode");
 
     }
 
